152988b3985696759c9acf84700f2bdc95c41e90
pesudo_github <fangjingyao8@gmail.com>
2021-11-08 13:35:51 +0100

    update server.js

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ var conn = mysql.createConnection({
 
 // verifier si la connection et bonne ou pas
 conn.connect((err) => {
-    if(!err){
+    if (!err) {
         console.log("La connection à la bdd est effectuée");
-    }
-    else {
+    } else {
         console.log("La connection à la BDD a échouée \n Erreur :" + JSON.stringify(err,undefined,2));
     }
 });
@@ -33,12 +32,9 @@ app.listen(port, () => console.log("L'api wikibrain tourne sur : http://localhos
 /// Utilisateur
 app.get('/utilisateurs', (req,res) => {
     conn.query('SELECT * FROM Utilisateur',(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -46,12 +42,9 @@ app.get('/utilisateurs', (req,res) => {
 
 app.get('/utilisateurs/:id', (req,res) => {
     conn.query('SELECT * FROM Utilisateur where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -59,12 +52,31 @@ app.get('/utilisateurs/:id', (req,res) => {
 
 app.delete('/utilisateurs/:id', (req,res) => {
     conn.query('DELETE FROM Utilisateur where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             res.send("Données parfaitement supprimées");
+        } else {
+            console.log(err);
         }
-        else
-        {
+    })
+});
+
+app.put('/utilisateurs', (req,res) => {
+    let utilisateurs = req.body;
+    conn.query('INSERT INTO Utilisateur (nom,prenom,username,mdp,id_role) VALUES (?,?,?,?,?)',[utilisateurs.nom,utilisateurs.prenom,utilisateurs.username,utilisateurs.mdp,utilisateurs.id_role],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/utilisateurs/:id', (req,res) => {
+    let utilisateurs = req.body;
+    conn.query('UPDATE Utilisateur SET nom = ? , prenom = ? , username = ? , mdp = ? WHERE id = ?',[utilisateurs.nom,utilisateurs.prenom,utilisateurs.username,utilisateurs.mdp],[req.params.id],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
             console.log(err);
         }
     })
@@ -74,38 +86,29 @@ app.delete('/utilisateurs/:id', (req,res) => {
 /// Role
 app.get('/roles', (req,res) =>{
     conn.query('SELECT * FROM Role',(err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
 });
 
 app.get('/roles/:id', (req,res) => {
-    conn.query('SELECT * FROM Roles where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+    conn.query('SELECT * FROM Role where id = ?',[req.params.id],(err,rows,fields) => {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
 });
 
 app.delete('/roles/:id', (req,res) => {
-    conn.query('DELETE FROM Roles where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+    conn.query('DELETE FROM Role where id = ?',[req.params.id],(err,rows,fields) => {
+        if (!err) {
             res.send("Données parfaitement supprimées");
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -114,16 +117,13 @@ app.delete('/roles/:id', (req,res) => {
 app.put('/roles', (req,res) => {
     let roles = req.body;
     conn.query('INSERT INTO Role (nom) VALUES (?)',[roles.nom],(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             console.log(rows);
             res.send({
                 "code":200,
                 "success":"Le role a bien été crée"
             });
-        }
-        else
-        {
+        } else {
             console.log(err);
             res.send({
                 "code":400,
@@ -136,10 +136,9 @@ app.put('/roles', (req,res) => {
 app.post('/roles/:id', (req,res) => {
     let role = req.body;
     conn.query('UPDATE Role SET nom = ? WHERE id = ?',[role.nom],[req.params.id],(err,rows,fields) => {
-        if(!err){
+        if (!err) {
             console.log(rows);
-        }
-        else {
+        } else {
             console.log(err);
         }
     })
@@ -149,12 +148,9 @@ app.post('/roles/:id', (req,res) => {
 /// Categorie
 app.get('/categories', (req,res) =>{
     conn.query('SELECT * FROM Categorie',(err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -162,12 +158,9 @@ app.get('/categories', (req,res) =>{
 
 app.get('/categories/:id', (req,res) => {
     conn.query('SELECT * FROM Categorie where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -175,12 +168,31 @@ app.get('/categories/:id', (req,res) => {
 
 app.delete('/categories/:id', (req,res) => {
     conn.query('DELETE FROM Categorie where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+        if (!err) {
             res.send("Données parfaitement supprimées");
+        } else {
+            console.log(err);
         }
-        else
-        {
+    })
+});
+
+app.put('/categories', (req,res) => {
+    let categories = req.body;
+    conn.query('INSERT INTO Categorie (nom) VALUES (?)',[categories.nom],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/categories/:id', (req,res) => {
+    let categories = req.body;
+    conn.query('UPDATE Categorie SET nom = ? WHERE id = ?',[categories.nom],[req.params.id],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
             console.log(err);
         }
     })
@@ -190,12 +202,9 @@ app.delete('/categories/:id', (req,res) => {
 /// Fiche
 app.get('/fiches', (req,res) =>{
     conn.query('SELECT * FROM Fiche',(err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -203,25 +212,41 @@ app.get('/fiches', (req,res) =>{
 
 app.get('/fiches/:id', (req,res) =>{
     conn.query('SELECT * FROM Fiche where id = ?',[req.params.id], (err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
 });
 
 app.delete('/fiches/:id', (req,res) => {
-    conn.query('DELETE Fiche where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+    conn.query('DELETE FROM Fiche where id = ?',[req.params.id],(err,rows,fields) => {
+        if (!err) {
             res.send("Données parfaitement supprimées");
+        } else {
+            console.log(err);
         }
-        else
-        {
+    })
+});
+
+app.put('/fiches', (req,res) => {
+    let fiches = req.body;
+    conn.query('INSERT INTO Fiche (titre,contenu,date_creation,id_categ,id_util) VALUES (?,?,?,?,?)',[fiches.titre,fiches.contenu,fiches.date_creation,fiches.id_categ,fiches.id_util],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/fiches/:id', (req,res) => {
+    let fiches = req.body;
+    conn.query('UPDATE Fiche SET titre = ? , contenu = ? WHERE id = ?',[fiches.titre,fiches.contenu],[req.params.id],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
             console.log(err);
         }
     })
@@ -231,12 +256,9 @@ app.delete('/fiches/:id', (req,res) => {
 /// Modification_fiche
 app.get('/modfiches', (req,res) =>{
     conn.query('SELECT * FROM Modification_fiche',(err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -244,25 +266,41 @@ app.get('/modfiches', (req,res) =>{
 
 app.get('/modfiches/:id', (req,res) =>{
     conn.query('SELECT * FROM Modification_fiche where id = ?',[req.params.id], (err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
 });
 
 app.delete('/modfiches/:id', (req,res) => {
-    conn.query('DELETE Modification_fiche where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+    conn.query('DELETE FROM Modification_fiche where id = ?',[req.params.id],(err,rows,fields) => {
+        if (!err) {
             res.send("Données parfaitement supprimées");
+        } else {
+            console.log(err);
         }
-        else
-        {
+    })
+});
+
+app.put('/modfiches', (req,res) => {
+    let modfiches = req.body;
+    conn.query('INSERT INTO Modification_fiche (date_motif,id_fiche,id_util) VALUES (?,?,?)',[modfiches.date_motif,modfiches.id_fiche,modfiches.id_util],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/modfiches/:id', (req,res) => {
+    let modfiches = req.body;
+    conn.query('UPDATE Modification_fiche SET date_motif = ? , id_fiche = ? , id_util = ? WHERE id = ?',[modfiches.date_motif,modfiches.id_fiche,modfiches.id_util],[req.params.id],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
             console.log(err);
         }
     })
@@ -272,12 +310,9 @@ app.delete('/modfiches/:id', (req,res) => {
 /// Commentaire_fiche
 app.get('/comfiches', (req,res) =>{
     conn.query('SELECT * FROM Commentaire_fiche',(err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
@@ -285,25 +320,41 @@ app.get('/comfiches', (req,res) =>{
 
 app.get('/comfiches/:id', (req,res) =>{
     conn.query('SELECT * FROM Commentaire_fiche where id = ?',[req.params.id], (err,rows,fields) =>{
-        if(!err)
-        {
+        if (!err) {
             res.send(rows);
-        }
-        else
-        {
+        } else {
             console.log(err);
         }
     })
 });
 
 app.delete('/comfiches/:id', (req,res) => {
-    conn.query('DELETE Commentaire_fiche where id = ?',[req.params.id],(err,rows,fields) => {
-        if(!err)
-        {
+    conn.query('DELETE FROM Commentaire_fiche where id = ?',[req.params.id],(err,rows,fields) => {
+        if (!err) {
             res.send("Données parfaitement supprimées");
+        } else {
+            console.log(err);
         }
-        else
-        {
+    })
+});
+
+app.put('/comfiches', (req,res) => {
+    let comfiches = req.body;
+    conn.query('INSERT INTO Commentaire_fiche (contenu,date_crea,id_fiche,id_util) VALUES (?,?,?,?)',[comfiches.contenu,comfiches.date_crea,comfiches.id_fiche,comfiches.id_util],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
+            console.log(err);
+        }
+    })
+});
+
+app.post('/comfiches/:id', (req,res) => {
+    let comfiches = req.body;
+    conn.query('UPDATE Commentaire_fiche SET contenu = ? WHERE id = ?',[comfiches.contenu],[req.params.id],(err,rows,fields) => {
+        if (!err) {
+            console.log(rows);
+        } else {
             console.log(err);
         }
     })
